045c47633716ac0c7e4cbad2bcbd641c19a44518
westofsky <westofsky159@gmail.com>
2024-01-05 14:33:44 +0900

    test: ctrl c 키보드 입력 감지

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	const [src, setSrc] = useState<string>('');
-	const handleClick = async (event: any) => {
+	const handleClick = async () => {
 		const isTextClipBoardItem = await navigator.clipboard.readText();
 		if (isTextClipBoardItem) {
 			console.log(isTextClipBoardItem);
@@ -20,9 +20,31 @@ function App() {
 			}
 		}
 	};
+
+	useEffect(() => {
+		const handleKeyDown = (event: {
+			preventDefault: () => void;
+			which: any;
+			keyCode: any;
+			ctrlKey: any;
+			metaKey: any;
+		}) => {
+			event.preventDefault();
+			const code = event.which || event.keyCode;
+
+			let charCode = String.fromCharCode(code).toLowerCase();
+			if ((event.ctrlKey || event.metaKey) && charCode === 'c') {
+				handleClick();
+			}
+		};
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => window.removeEventListener('keydown', handleKeyDown);
+	}, []);
 	return (
 		<>
 			<button onClick={handleClick}>테스트</button>
+			<div>테스트2</div>
 			<img src={src} />
 		</>
 	);
