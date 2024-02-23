7112933b35e6197d0afd5a5f8ae3dcf38352b8de
westofsky <westofsky159@gmail.com>
2024-02-24 00:16:37 +0900

    feat: 초마다 복사 감지 기능 추가

--- src/background/index.ts
+++ src/background/index.ts
@@ -5,3 +5,13 @@ chrome.runtime.onMessage.addListener((request) => {
     console.log('background has received a message from popup, and count is ', request?.count)
   }
 })
+async function readClipboardText() {
+  try {
+    const text = await navigator.clipboard.readText()
+    console.log('클립보드에서 읽은 텍스트:', text)
+  } catch (err) {
+    console.error('클립보드 접근 권한이 없거나, 다른 오류 발생:', err)
+  }
+}
+
+setInterval(readClipboardText, 5000)
